e2ceb87e472214528a03b8b1bde5a7c8434c4447
AlbertFaust <robert.mcconnell3@mail.dcu.ie>
2015-12-12 22:51:07 +0000

    Create Day1.1.cpp

---
+++ Day1.1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int floorNum(char* array){
+	int fl;
+	for(int i = 0; array[i] != '\0'; i++){
+		if(array[i] == '(')
+			fl++;
+		else if(array[i] == ')')
+			fl--;
+	}
+	return fl;
+}
+int main(){
+	int size = 9001;
+	char* array = new char[size];
+	ifstream fin("input.txt");
+	int position = 0;
+	
+    if(fin.is_open())
+    {
+		while(!fin.eof() && position < size)
+        {
+        	fin.get(array[position]);
+            position++;
+        }
+        array[position-1] = '\0';
+	}
+	cout << floorNum(array) << endl;
+}
